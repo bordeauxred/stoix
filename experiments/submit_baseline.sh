301a2c4061bc21adde0f82c90be12888023b2aeb
bordeauxred <2robert.mueller@gmail.com>
2026-01-15 19:24:34 +0100

    update slurm scirpt

--- experiments/submit_baseline.sh
+++ experiments/submit_baseline.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=depth_baseline
+#SBATCH --partition=kisski
 #SBATCH --array=0-44%4
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=4:00:00
+#SBATCH --time=04:00:00
+#SBATCH --gres=gpu:A100:1
 #SBATCH --output=logs/depth_baseline_%A_%a.out
 #SBATCH --error=logs/depth_baseline_%A_%a.err
+#SBATCH -C inet
 
 mkdir -p logs
 
